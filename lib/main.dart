7ab3aede748eed6b17ef69c58fa1e2f8af0efd85
Brieuc Courapié <brieuc.courapie@free.fr>
2023-01-04 17:03:45 +0100

    dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -132,10 +133,44 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         // Page "Parametre"
         Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text('Paramètre'),
-        )
+            color: Colors.blue,
+            alignment: Alignment.center,
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+              children: <Widget>[
+                DeBox(
+                    faceDe: "1",
+                    description: "La face 1 du dé",
+                    valeur: 1,
+                    image: "face1.png"),
+                DeBox(
+                    faceDe: "2",
+                    description: "La face 2 du dé",
+                    valeur: 2,
+                    image: "face2.png"),
+                DeBox(
+                    faceDe: "3",
+                    description: "La face 3 du dé",
+                    valeur: 3,
+                    image: "face3.png"),
+                DeBox(
+                    faceDe: "4",
+                    description: "La face 4 du dé",
+                    valeur: 4,
+                    image: "face4.png"),
+                DeBox(
+                    faceDe: "5",
+                    description: "La face 5 du dé",
+                    valeur: 5,
+                    image: "face5.png"),
+                DeBox(
+                    faceDe: "5",
+                    description: "La face 6 du dé",
+                    valeur: 6,
+                    image: "face6.png"),
+              ],
+            )),
       ][currentPageIndex],
       // Boutton Incrémentation
       floatingActionButton: Stack(
@@ -184,6 +219,7 @@ class _MyHomePageState extends State<MyHomePage> {
             label: 'Accueil',
           ),
           NavigationDestination(
+            selectedIcon: Text("="),
             icon: Icon(Icons.start),
             label: 'Page suivante',
           ),
